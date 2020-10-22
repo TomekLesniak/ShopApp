28b2dad4a4b93c60e9d9ec09cba835231b093bdd
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-22 16:35:59 +0200

    Home Screen & custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ShopApp/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ShopApp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Lato'
       ),
-      home: MyHomePage(),
+      routes: {
+        '/': (ctx) => ProductsOverviewScreen()
+      },
     );
   }
 }
