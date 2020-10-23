21fe49edc2360018a01ccddedcf76cb317862958
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-23 23:08:20 +0200

    Detailed product screen

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id, this.price, this.quantity, this.title, this.productId);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(child: Padding(padding: EdgeInsets.all(8),child: FittedBox(child: Text('\$$price')))),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
+    final cart = Provider.of<Cart>(context, listen: false);
+    return Dismissible(
+      key: ValueKey(id),
+      onDismissed: (direction) {
+        cart.removeItem(productId);
+      },
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(Icons.delete, color: Colors.white, size: 40),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+      ),
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+                child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: FittedBox(child: Text('\$$price')))),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
+          ),
         ),
       ),
     );
