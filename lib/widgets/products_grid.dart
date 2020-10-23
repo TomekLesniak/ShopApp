20e00d42a58838159ed7b815c4c83a41a8ff912c
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-23 13:20:09 +0200

    Change Product to provider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,8 +5,6 @@ import '../widgets/product_item.dart';
 import '../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -22,8 +20,10 @@ class ProductsGrid extends StatelessWidget {
         mainAxisSpacing: 10,
       ),
       itemBuilder: (ctx, index) {
-        return ProductItem(products[index].id,
-            products[index].title, products[index].imageUrl);
+        return ChangeNotifierProvider(
+          create: (ctx) => products[index],
+          child: ProductItem(),
+        );
       },
     );
   }
