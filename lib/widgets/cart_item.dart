69c8228743ae4b44ef3cb5b0d952f02c23786473
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-25 17:37:44 +0100

    Confirm dismiss

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -17,6 +17,29 @@ class CartItem extends StatelessWidget {
     final cart = Provider.of<Cart>(context, listen: false);
     return Dismissible(
       key: ValueKey(id),
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('Do you want to remove this item from the cart?'),
+                  actions: [
+                    FlatButton(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Yes'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                    ),
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         cart.removeItem(productId);
       },
